716753e51ffc78deb61d4a4dcdadb332aa7d44d9
superznake <108449247+superznake@users.noreply.github.com>
2025-06-10 12:24:05 +0300

    .

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 app = FastAPI()
 
 
-@app.get("/minecraft/how")
+@app.get("/minecraft/how", response_class=HTMLResponse)
 async def root():
-    result = ("Итак, ты хочешь поиграть на моем сервачке. Как же это сделать? Все предельно просто:"
-              "\n1 - тебе нужно поставить сборку 'Pokehaan Craft 2-2.10.0'"
-              "\n2 - добавить сервер вот его адрес 'superznake.store:25565'"
-              "\n3 - написать боту /launch, если сервер не запущен."
-              "\nГотово."
-              "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-              "\nP.S. Это сборка с покемонами, чтобы начать нужно выбрать себе стартового и подтвердить выбор")
-    return result
+    html_path = Path("templates/index.html")
+    html_content = html_path.read_text(encoding="utf-8")
+    return HTMLResponse(content=html_content)
